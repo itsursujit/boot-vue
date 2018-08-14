e447831de0bb0d73827afa210f8ef3e43dade587
Sujit Baniya <mr.sujit.baniya@gmail.com>
2018-08-14 13:56:23 +0545

    Initial Commit

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -4,11 +4,10 @@
  * includes Vue and other libraries. It is a great starting point when
  * building robust, powerful web applications using Vue and Laravel.
  */
-require('./bootstrap');
-import Vue from 'vue'
-import BootstrapVue from 'bootstrap-vue'
 
-Vue.use(BootstrapVue);
+require('./bootstrap');
+
+window.Vue = require('vue');
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -21,3 +20,34 @@ Vue.component('example-component', require('./components/ExampleComponent.vue'))
 const app = new Vue({
     el: '#app'
 });
+
+
+// Bulma NavBar Burger Script
+document.addEventListener('DOMContentLoaded', function () {
+    // Get all "navbar-burger" elements
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+    
+    // Check if there are any navbar burgers
+    if ($navbarBurgers.length > 0) {
+        
+        // Add a click event on each of them
+        $navbarBurgers.forEach(function ($el) {
+            $el.addEventListener('click', function () {
+                
+                // Get the target from the "data-target" attribute
+                let target = $el.dataset.target;
+                let $target = document.getElementById(target);
+                
+                // Toggle the class on both the "navbar-burger" and the "navbar-menu"
+                $el.classList.toggle('is-active');
+                $target.classList.toggle('is-active');
+                
+            });
+        });
+    }
+    
+});
+
+
+
+require('./bulma-extensions');
