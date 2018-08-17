7fa990de02d229f8d3839b74dbfd3b69dc6caf85
Sujit Baniya <itsursujit@gmail.com>
2018-08-17 14:56:15 +0545

    Changes

--- Modules/Site/Http/Middleware/SiteMiddleware.php
+++ Modules/Site/Http/Middleware/SiteMiddleware.php
@@ -16,6 +16,15 @@ class SiteMiddleware
      */
     public function handle(Request $request, Closure $next)
     {
+        $host   = request()->getHttpHost();
+        $domain = explode('.', $host);
+        // Log::info('1) Host: ' . $host); // webdeveloper.im
+        $bottom_host_name = $host;
+        if (count($domain) > 2)
+        {
+            $bottom_host_name = $domain[count($domain) - 2] . "." . $domain[count($domain) - 1];
+        }
+        dd($domain);
         return $next($request);
     }
 }
